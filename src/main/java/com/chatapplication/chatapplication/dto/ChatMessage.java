32a826ed1cd6b0aa5429cd8b462bc48f643cfa8e
rkrukshan <rukshan1122@gmail.com>
2025-10-27 22:50:45 +0530

    Add dto ChatMessage

--- src/main/java/com/chatapplication/chatapplication/dto/ChatMessage.java
+++ src/main/java/com/chatapplication/chatapplication/dto/ChatMessage.java
@@ -1,4 +1,15 @@
 package com.chatapplication.chatapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessage {
+    private String message;
+    private String timestamp;
+    private  String userName;
+    private MessageType messageType;
 }
