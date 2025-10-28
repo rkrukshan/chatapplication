db331089db516d5b06e231c68455bb25def0a436
rkrukshan <rukshan1122@gmail.com>
2025-10-28 16:54:19 +0530

    Create ChatMessage method in ChatController

--- src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
+++ src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
@@ -1,8 +1,25 @@
 package com.chatapplication.chatapplication.controller;
 
+import com.chatapplication.chatapplication.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
+@Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final RedisTemplate redisTemplate;
+
+    @MessageMapping("/chat.send")
+    public ChatMessage sendChatMessage(@Payload ChatMessage chatMessage){
+    chatMessage.setTimestamp(LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+    redisTemplate.convertAndSend("chat", chatMessage);
+    }
 
 }
