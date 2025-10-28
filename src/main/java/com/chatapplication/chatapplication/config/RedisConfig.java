6edd8dc56556860fa63623e1b086255f7e1a639f
rkrukshan <rukshan1122@gmail.com>
2025-10-28 14:15:01 +0530

    Add RedisConfig

--- src/main/java/com/chatapplication/chatapplication/config/RedisConfig.java
+++ src/main/java/com/chatapplication/chatapplication/config/RedisConfig.java
@@ -2,11 +2,28 @@ package com.chatapplication.chatapplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chat");
+    }
 
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        return template;
+    }
 
 }
