6c577a930e46ae7e0da4df90de42c915eae226a4
rkrukshan <rukshan1122@gmail.com>
2025-10-28 22:52:06 +0530

    Update ChatController

--- src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
+++ src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
@@ -32,7 +33,8 @@ public class ChatController {
     {
         headerAccessor.getSessionAttributes().put("Username", chatMessage.getUserName());
         chatMessage.setMessageType(MessageType.JOIN);
-        chatMessage.setMessage(chatMessage.getUserName());
+        chatMessage.setMessage(chatMessage.getUserName() + " Joined the Chat");
+        chatMessage.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
         log.info("User Joined: {}", chatMessage.getUserName());
 
         redisTemplate.convertAndSend("chat", chatMessage);
