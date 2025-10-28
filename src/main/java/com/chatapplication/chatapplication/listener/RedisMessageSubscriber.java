d5f2d67e70d06d6fbe42a8b7171b7f66747f6416
rkrukshan <rukshan1122@gmail.com>
2025-10-28 19:30:00 +0530

    Implement onMessage method

--- src/main/java/com/chatapplication/chatapplication/listener/RedisMessageSubscriber.java
+++ src/main/java/com/chatapplication/chatapplication/listener/RedisMessageSubscriber.java
@@ -1,7 +1,12 @@
 package com.chatapplication.chatapplication.listener;
 
+import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
 public class RedisMessageSubscriber implements MessageListener {
 
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+
+    }
 }
