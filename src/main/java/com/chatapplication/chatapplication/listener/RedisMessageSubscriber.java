d2fce67137503ae23df486f6450aef33d15a8eba
rkrukshan <rukshan1122@gmail.com>
2025-10-28 19:26:38 +0530

    Create RedisMessageSubscriber

---
+++ src/main/java/com/chatapplication/chatapplication/listener/RedisMessageSubscriber.java
@@ -0,0 +1,7 @@
+package com.chatapplication.chatapplication.listener;
+
+import org.springframework.data.redis.connection.MessageListener;
+
+public class RedisMessageSubscriber implements MessageListener {
+
+}
