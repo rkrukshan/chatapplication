bdc77da74d7a7cafa541e013f366161d6edb1166
rkrukshan <rukshan1122@gmail.com>
2025-10-28 14:03:12 +0530

    Create RedisConfig

--- src/main/java/com/chatapplication/chatapplication/config/RedisConfig.java
+++ src/main/java/com/chatapplication/chatapplication/config/RedisConfig.java
@@ -1,7 +1,12 @@
 package com.chatapplication.chatapplication.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.listener.ChannelTopic;
 
 @Configuration
 public class RedisConfig {
+
+
+
 }
