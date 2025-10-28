3ac3f73c2afd1abeccd6d4f1266a58e947db7090
rkrukshan <rukshan1122@gmail.com>
2025-10-28 22:26:59 +0530

    Add WebsocketEventListener

--- src/main/java/com/chatapplication/chatapplication/listener/WebsocketEventListener.java
+++ src/main/java/com/chatapplication/chatapplication/listener/WebsocketEventListener.java
@@ -1,4 +1,34 @@
 package com.chatapplication.chatapplication.listener;
 
+import com.chatapplication.chatapplication.dto.ChatMessage;
+import com.chatapplication.chatapplication.dto.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebsocketEventListener {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @EventListener
+    public  void handleWebSocketDisconnectListner(SessionDisconnectEvent event)
+    {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setMessageType(MessageType.LEAVE);
+            chatMessage.setUserName(username);
+            chatMessage.setMessage(username + " Left the Chat");
+            log.info("User disconnected: {}", username);
+            redisTemplate.convertAndSend("chat", chatMessage);
+        }
+    }
 }
