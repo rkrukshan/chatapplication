90f98d3495cecf876f55da62634164faf53eecfd
rkrukshan <rukshan1122@gmail.com>
2025-10-28 19:52:09 +0530

    Add RedisMessageSubscriber

--- src/main/java/com/chatapplication/chatapplication/listener/RedisMessageSubscriber.java
+++ src/main/java/com/chatapplication/chatapplication/listener/RedisMessageSubscriber.java
@@ -1,12 +1,33 @@
 package com.chatapplication.chatapplication.listener;
 
+import com.chatapplication.chatapplication.dto.ChatMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class RedisMessageSubscriber implements MessageListener {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
+    String publishedMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+
+    try{
+        ChatMessage chatMessage = objectMapper.readValue(publishedMessage, ChatMessage.class);
+        simpMessageSendingOperations.convertAndSend("/topic/public", chatMessage);
+    } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+    }
 
     }
 }
