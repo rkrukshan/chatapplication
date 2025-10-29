d4fc3d09152e5b545d667d522a923d3125c52983
rkrukshan <rukshan1122@gmail.com>
2025-10-29 13:10:07 +0530

    Add Validation to ChatMessage dto

--- src/main/java/com/chatapplication/chatapplication/dto/ChatMessage.java
+++ src/main/java/com/chatapplication/chatapplication/dto/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.chatapplication.chatapplication.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessage {
+
+    @NotBlank(message = "Message content cannot be empty")
     private String message;
+
     private String timestamp;
-    private  String userName;
+
+    @NotBlank(message = "Username cannot be empty")
+    private String userName;
+
+    @NotNull(message = "Message type cannot be null")
     private MessageType messageType;
 }
