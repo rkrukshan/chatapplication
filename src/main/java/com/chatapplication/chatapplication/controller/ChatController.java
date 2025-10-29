06a11a6a915998ee70f6ca4dd84787edb82fe851
rkrukshan <rukshan1122@gmail.com>
2025-10-29 13:14:19 +0530

    Update ChatController

--- src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
+++ src/main/java/com/chatapplication/chatapplication/controller/ChatController.java
@@ -1,42 +1,30 @@
 package com.chatapplication.chatapplication.controller;
 
 import com.chatapplication.chatapplication.dto.ChatMessage;
-import com.chatapplication.chatapplication.dto.MessageType;
+import com.chatapplication.chatapplication.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class ChatController {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChatService chatService;
 
     @MessageMapping("/chat.send")
     public ChatMessage sendChatMessage(@Payload ChatMessage chatMessage) {
-        chatMessage.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
-        redisTemplate.convertAndSend("chat", chatMessage);
-        return chatMessage;
+        log.debug("Received chat message from user: {}", chatMessage.getUserName());
+        return chatService.processAndSendChatMessage(chatMessage);
     }
 
     @MessageMapping("/chat.adduser")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getUserName());
-        chatMessage.setMessageType(MessageType.JOIN);
-        chatMessage.setMessage(chatMessage.getUserName() + " joined the chat");
-        chatMessage.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
-
-        log.info("User joined: {}", chatMessage.getUserName());
-
-        redisTemplate.convertAndSend("chat", chatMessage);
-        return chatMessage;
+        log.debug("Adding user to chat: {}", chatMessage.getUserName());
+        return chatService.addUserToChat(chatMessage, headerAccessor);
     }
 }
